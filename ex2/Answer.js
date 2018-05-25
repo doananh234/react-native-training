19830301df30773b42cfef7d066637bffdbfc6ef
DoanAnh <doananh234@gmail.com>
2018-05-25 20:51:01 +0700

    fix  Answer component

--- ex2/Answer.js
+++ ex2/Answer.js
@@ -4,23 +4,19 @@ import {
   View,
 } from "react-native";
 
-class Answer extends Component {
-  onPress = () => {
-    this.props.onPress(this.props.data);
-  }
-  render() {
-    let { text } = this.props.data
-    return (
-      <View
-        style={[
-          { alignItems: "center", },
-          this.props.isSelected && { backgroundColor: '#ddd111' }
-        ]}>
-        <Text onPress={this.onPress}>
-          {text}
-        </Text>
-      </View>
-    )
-  }
-}
+const Answer = ({ onPress, isSelected, data }) => {
+  const { text } = data;
+  return (
+    <View
+      style={[
+        { alignItems: "center", },
+        isSelected && { backgroundColor: '#ddd111' }
+      ]}>
+      <Text onPress={() => onPress(data)}>
+        {text}
+      </Text>
+    </View>
+  )
+};
+
 export default Answer;
